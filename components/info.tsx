4d68e96659221663f5f81d00445f11dd91a501de
johnny shafir <97397410+Josnon@users.noreply.github.com>
2022-11-25 15:20:07 +0200

    fixes

--- components/info.tsx
+++ components/info.tsx
@@ -23,62 +23,62 @@ const Info = (setInfo: any, updateInfo: any): JSX.Element => {
         </button>
         <div className={Styles.howtouse}>
           <h2>How To Use</h2>
-          <p className={Styles.settings_box}>
+          <section className={Styles.settings_box}>
             <BsGear />
             <span>settings</span>if you want to change theme or font style
-          </p>
-          <p className={Styles.history_info_mode_box}>
+          </section>
+          <section className={Styles.history_info_mode_box}>
             <BsClockHistory />
             <span>history mode</span>
             on/off to view the operation log <br />
             you can view the log even if your mode was off
-          </p>
-          <p className={Styles.light_box}>
+          </section>
+          <section className={Styles.light_box}>
             <BsLightbulb />
             <span>light</span>make the screen more lighter
-          </p>
-          <p className={Styles.info_box}>
+          </section>
+          <section className={Styles.info_box}>
             <BsInfoCircle />
             <span>info</span>if you dont remmber something or you want to see
             the vresion
-          </p>
-          <p className={Styles.scientific_info_mode_box}>
+          </section>
+          <section className={Styles.scientific_info_mode_box}>
             <TbMath />
             <span>scientific</span>
             <h5>Off</h5>not using math rules, for fast & basic use
             <h5>On</h5>using math rules
-          </p>
-          <p className={Styles.external_box}>
+          </section>
+          <section className={Styles.external_box}>
             <AiOutlineCloudSync />
             <span>external mode</span>if you switch it the calculations will be
             on <em>api.mathjs.org</em>
-          </p>
-          <p className={Styles.del_box}>
+          </section>
+          <section className={Styles.del_box}>
             <BsBackspace />
             <span>delete</span> delete your last digit
-          </p>
-          <p className={Styles.clear_box}>
+          </section>
+          <section className={Styles.clear_box}>
             <TbLetterC />
             <span>clear</span>delete everthing including your history log so
             care..
-          </p>
+          </section>
         </div>
         <div className={Styles.usedlang}>
-          <p>
+          <section>
             This is a simple calculator app that can be used to perform basic or
             scientific calculations.
             <br />
             <br /> The app is built using React and Typescript.
             <br /> <br />
             The app is fully responsive and can be used on any device.
-          </p>
+          </section>
         </div>
         <div className={Styles.devifno}>
           <h3>Dev info</h3>
           <div className={Styles.devinfo_content}>
-            <p>By: Jonik Shafir</p>
-            <p>Awasome Calculator</p>
-            <p>version #TODO </p>
+            <section>By: Jonik Shafir</section>
+            <section>Awasome Calculator</section>
+            <section>version #TODO </section>
           </div>
         </div>
       </div>
